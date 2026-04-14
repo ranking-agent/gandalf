d9651be2e95e7aed96f1fe04e6704e942ae4d985
Max Wang <max@covar.com>
2026-04-14 09:41:22 -0400

    Bump patch version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 
 setup(
     name="gandalf",
-    version="0.3.2",
+    version="0.3.3",
     author="Max Wang",
     author_email="max@covar.com",
     description="Fast 3-hop path finding in large knowledge graphs",
